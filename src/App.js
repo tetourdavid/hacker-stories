d9e4f910435d4791fc57362259fac3d6e0a3da58
David Tetour <34492147+tetourdavid@users.noreply.github.com>
2022-07-30 16:16:55 +0200

    React JSX chapter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 
+const welcome = {
+  greeting: 'Hi',
+  title: 'React',
+};
+
 function App() {
   // todo add logic here
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>{welcome.greeting} {welcome.title}!</h1>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" /  >
     </div>
   );
 }
