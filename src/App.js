02184b50e1b078255058d67780e30c11a6c00ead
David Tetour <34492147+tetourdavid@users.noreply.github.com>
2022-07-31 12:16:48 +0200

    Meet another React Component chapter

--- src/App.js
+++ src/App.js
@@ -25,26 +25,34 @@ const list = [
 ];
 
 function App() {
-  // todo add logic here
   return (
     <div className="App">
       <h1>{welcome.greeting} {welcome.title}!</h1>
+      <Search />
+      <hr />
+      <List />
+    </div>
+  );
+}
+
+function List() {
+  return (
+    <ul>{list.map(item => (
+      <li key={item.objectID}>
+        <span><a href={item.url}>{item.title}</a></span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </li>
+    ))}</ul>
+  );
+}
+
+function Search() {
+  return (
+    <div>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
-
-      <hr />
-
-      <ul>
-        {list.map(item => (
-          <li key={item.objectID}>
-            <span><a href={item.url}>{item.title}</a></span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </li>)
-        )}
-      </ul>
-
     </div>
   );
 }
